f763daba6114658984064d9079d702ba74d887c3
Miles Niemeyer <milesniemeyer@MJN-MacBook-Pro.local>
2023-11-14 20:19:34 -0500

    Working list models endpoint/method

--- ollama-py/app.py
+++
@@ -1,9 +0,0 @@
-from ollamaapi import OllamaAPI
-
-url = 'http://localhost:11434'
-
-ollama_api = OllamaAPI(url)
-
-prompt = "What was perestroika?"
-get_response = ollama_api.generate_completion('orca-mini:3b', prompt)
-print(get_response['response'])
--- ollama-py/ollamaapi.py
+++ ollama-py/ollamaapi.py
@@ -6,7 +6,7 @@ class OllamaAPI:
 
     # Helper function that uses requests to make the API calls
     def _make_request(self, method, endpoint, data=None):
-        headers = {"Content-Type": "application/json"}
+        headers = { "Content-Type": "application/json" }
         url = f'{self.base_url}/{endpoint}'
 
         try:
@@ -14,14 +14,14 @@ class OllamaAPI:
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error making API request: {e}")
-            print(f"HTTP Status Code: {response.status_code}")
-            print(f"Response content: {response.content}")
+            print(f'Error making API request: {e}')
+            print(f'HTTP Status Code: {response.status_code}')
+            print(f'Response content: {response.content}')
             return None
 
     # Generates a completion by taking in a model and prompt
     def generate_completion(self, model:str, prompt:str, format:str=None, options:str=None, system:str=None, template:str=None, context:str=None, stream:bool=False, raw:str=False):
-        endpoint = "api/generate"
+        endpoint = 'api/generate'
         payload = {
             "model": model,
             "prompt": prompt,
@@ -33,4 +33,19 @@ class OllamaAPI:
             "stream": stream,
             "raw": raw
         }
-        return self._make_request("POST", endpoint, data=payload)
+
+        return self._make_request('POST', endpoint, data=payload)
+    
+    def list_models(self):
+        endpoint = 'api/tags'
+        return self._make_request('GET', endpoint)
+    
+    def pull_model(self, name:str, insecure:bool=False, stream:bool=False):
+        endpoint = 'api/pull'
+        payload = {
+            "name": name
+            #"insecure": insecure,
+            #"stream": stream
+        }
+
+        return self._make_request('POST', endpoint, data=payload)
